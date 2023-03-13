df5f4dde734fa041a0cbe7bd335f5613461ec2c2
Chia Sẻ Vip <127665309+chiasevip@users.noreply.github.com>
2023-03-14 00:59:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,119 @@
 <script>
-/*! Minimalist Web Notepad | https://github.com/pereorga/minimalist-web-notepad */
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
 
-function uploadContent() {
+//List of fontlist
+let fontList = [
+  "Arial",
+  "Verdana",
+  "Times New Roman",
+  "Garamond",
+  "Georgia",
+  "Courier New",
+  "cursive",
+];
 
-    // If textarea value changes.
-    if (content !== textarea.value) {
-        var temp = textarea.value;
-        var request = new XMLHttpRequest();
-        request.open('POST', window.location.href, true);
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+//Initial Settings
+const initializer = () => {
+  //function calls for highlighting buttons
+  //No highlights for link, unlink,lists, undo,redo since they are one time operations
+  highlighter(alignButtons, true);
+  highlighter(spacingButtons, true);
+  highlighter(formatButtons, false);
+  highlighter(scriptButtons, true);
 
-        request.onload = function() {
-            if (request.readyState === 4) {
+  //create options for font names
+  fontList.map((value) => {
+    let option = document.createElement("option");
+    option.value = value;
+    option.innerHTML = value;
+    fontName.appendChild(option);
+  });
 
-                // Request has ended, check again after 1 second.
-                content = temp;
-                setTimeout(uploadContent, 1000);
-            }
+  //fontSize allows only till 7
+  for (let i = 1; i <= 7; i++) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.innerHTML = i;
+    fontSizeRef.appendChild(option);
+  }
+
+  //default size
+  fontSizeRef.value = 3;
+};
+
+//main logic
+const modifyText = (command, defaultUi, value) => {
+  //execCommand executes command on selected text
+  document.execCommand(command, defaultUi, value);
+};
+
+//For basic operations which don't need value parameter
+optionsButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    modifyText(button.id, false, null);
+  });
+});
+
+//options that require value parameter (e.g colors, fonts)
+advancedOptionButton.forEach((button) => {
+  button.addEventListener("change", () => {
+    modifyText(button.id, false, button.value);
+  });
+});
+
+//link
+linkButton.addEventListener("click", () => {
+  let userLink = prompt("Enter a URL");
+  //if link has http then pass directly else add https
+  if (/http/i.test(userLink)) {
+    modifyText(linkButton.id, false, userLink);
+  } else {
+    userLink = "http://" + userLink;
+    modifyText(linkButton.id, false, userLink);
+  }
+});
+
+//Highlight clicked button
+const highlighter = (className, needsRemoval) => {
+  className.forEach((button) => {
+    button.addEventListener("click", () => {
+      //needsRemoval = true means only one button should be highlight and other would be normal
+      if (needsRemoval) {
+        let alreadyActive = false;
+
+        //If currently clicked button is already active
+        if (button.classList.contains("active")) {
+          alreadyActive = true;
         }
 
-        request.onerror = function() {
-
-            // Try again after 1 second.
-            setTimeout(uploadContent, 1000);
+        //Remove highlight from other buttons
+        highlighterRemover(className);
+        if (!alreadyActive) {
+          //highlight clicked button
+          button.classList.add("active");
         }
+      } else {
+        //if other buttons can be highlighted
+        button.classList.toggle("active");
+      }
+    });
+  });
+};
 
-        // Send the request.
-        request.send('text=' + encodeURIComponent(temp));
+const highlighterRemover = (className) => {
+  className.forEach((button) => {
+    button.classList.remove("active");
+  });
+};
 
-        // Make the content available to print.
-        printable.removeChild(printable.firstChild);
-        printable.appendChild(document.createTextNode(temp));
-    }
-    else {
-
-        // Content has not changed, check again after 1 second.
-        setTimeout(uploadContent, 1000);
-    }
-}
-
-var textarea = document.getElementById('content');
-var printable = document.getElementById('printable');
-var content = textarea.value;
-
-// Make the content available to print.
-printable.appendChild(document.createTextNode(content));
-
-// Enable TABs to indent. Based on https://stackoverflow.com/a/14166052/1391963
-textarea.onkeydown = function(e) {
-    if (e.keyCode === 9 || e.which === 9) {
-        e.preventDefault();
-        var s = this.selectionStart;
-        this.value = this.value.substring(0, this.selectionStart) + '\t' + this.value.substring(this.selectionEnd);
-        this.selectionEnd = s + 1;
-    }
-}
-
-textarea.focus();
-uploadContent();
+window.onload = initializer();
 </script>
